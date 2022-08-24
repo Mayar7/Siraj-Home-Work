9f993582745bb7d57211fd084de1d1c0f8873cb1
Mayar7 <85497553+Mayar7@users.noreply.github.com>
2022-08-24 11:40:23 +0300

    Add files via upload

---
+++ SirajHomeWork/src/com/javapapers/designpattern/Subject.java
@@ -0,0 +1,18 @@
+package com.javapapers.designpattern;
+
+import java.io.IOException;
+
+public interface Subject {
+
+
+	
+	public char getButton();
+	public void setButton(char button);
+	public void FuncthionButtonA(Observer observerButton);
+	public void FuncthionButtonB(Observer observerButton) throws IOException;
+	public void FuncthionButtonC(Observer observerButton);
+
+	
+	
+
+}
